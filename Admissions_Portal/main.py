d9bad1264f4feb4fe5a8d92efca5fb6c919e097c
Yoofi Setsie Awotwi <ysawotwi8@gmail.com>
2024-08-04 07:28:11 +0000

    Fixed the backend
    can write to a file. what is left is to check of data is already there but I would leave that for another time

--- Admissions_Portal/main.py
+++ Admissions_Portal/main.py
@@ -2,48 +2,51 @@ import random
 
 file_name = "students_admissions.txt"
 # intialising for the points the applicant will have
-
 points = 0
+
 # data from text file
 profile = {}
 contact = {}
 education = {}
-essay = {}
+personal_essay = {}
 activities = {}
+
+user_id = ''
 
 
 def id_number():
     # generates a random number as the id number of the student just in case they have same names with the other person
     # assuming that not no more than 5000 students will apply
-    id_number = random.randint(1000, 5000)
-    return id_number
-
+    return str(random.randint(1000, 5000))
 
 def account_login():
+    id_num = ''
+    email_address = ''
+    full_name = ''
+
     log_in = input('Do you have an account (yes/no): ')
     if log_in == 'yes':
         id_num = input('Enter your ID number: ')
-        if id_number in profile:
-            print(f'Welcome back, {profile[id_number][0]}')
-            application_stage()
+        if id_num in profile.keys():
+            print(f'Welcome back, {profile[id_num][0]}')
+            # application_stage()
         else:
             print('ID number not found. Please create an account')
             full_name = input('Enter your full name: ')
             email_address = input('Enter your email address: ')
-            # id_num = id_number()
-            print(f'Your ID Number is {id_number()},Please always remember your ID number.')
+            id_num = id_number()
+            print(f'Your ID Number is {id_num},Please always remember your ID number.')
             print(f'Welcome {full_name} to Leather VIII Admissions Portal, your account has been created.')
-            application_stage()
+            # application_stage()
     else:
         full_name = input('Enter your full name: ')
         email_address = input('Enter your email address: ')
-        # id_num = id_number()
-        print(f'Your ID Number is {id_number()},Please always remember your ID number')
+        id_num = id_number()
+        print(f'Your ID Number is {id_num},Please always remember your ID number')
         print(f'Welcome {full_name} to Leather VIII Admissions Portal, your account has been created')
 
-        application_stage()
-
-
+    profile[id_num] = [full_name, email_address]
+    return id_num
 def load_from_file():
     try:
         fout = open('admission_management.txt', 'r')
@@ -64,45 +67,58 @@ def load_from_file():
                 elif stages == 2:
                     education[identification_number] = individual_data
                 elif stages == 3:
-                    essay[identification_number] = individual_data
+                    personal_essay[identification_number] = individual_data
                 elif stages == 4:
                     activities[identification_number] = individual_data
 
                 stages += 1
+        fout.close()
     except FileNotFoundError:
-        pass
+        open('admission_management.txt', 'x')
 
 
-def save_to_file(profile,contact, education, essay, activities):
+def save_to_file(profile, contact, education, essay, activities):
+    fin=None
+
     try:
-        fos = open('students_admissions.txt', "w")
+        fin = open('students_admissions.txt', "w")
+        print('file opened')
 
     except IOError:
         print("Error creating file")
+    except:
+        print("File does not exist")
 
-    for id_number in profile:
+    for id_number, elements in profile.items():
+        '''
+            profile={
+                71972027=[yoofi setsie
+            }
+        '''
         # Create a list of parts for this ID number.
         # Adds the profile information.
-        parts = [f"Profile: {profile[id_number][0]}"]
+        add = ",".join(elements)
+        parts = [f"{add}"]
 
         # Add the contact information.
-        parts.append(
-            f"Contact: {contact[id_number][0]},{contact[id_number][1]},{contact[id_number][2]},{contact[id_number][3]}")
+        add = ",".join(contact[id_number])
+        parts.append(f"{add}")
 
         # Add the education information.
-        parts.append(f"Education: {education[id_number][0]},{education[id_number][1]},{education[id_number][2]}")
+        add = ",".join(education[id_number])
+        parts.append(f"{add}")
 
         # Add the essay information.
-        parts.append(f"Essay: {essay[id_number][0]}")
+        parts.append(f"{essay[id_number]}")
 
         # Add the activities information
-        parts.append(f"Activities: {activities[id_number][0]}")
+        add = ",".join(activities[id_number])
+        parts.append(f"{add}")
 
         # Join the parts with semicolons and write them to the file followed by a newline character.
-        fos.write(";".join(parts) + "\n")
-        print('here')
+        fin.write(";".join(parts) + "\n")
 
-    fos.close()
+    fin.close()
 
 
 # print(profile)
@@ -114,12 +130,11 @@ def save_to_file(profile,contact, education, essay, activities):
 entries = [profile,
            contact,
            education,
-           essay,
-           essay,
+           personal_essay,
            activities]
 
 
-def application_stage():
+def application_stage(user_id):
     # id_num = id_number()
     # identification = {full_name: id_number}
 
@@ -140,26 +155,26 @@ def application_stage():
             gender = input('Enter your gender(male/female): ')
             nationality = input('Enter your nationality: ')
 
-            # data = {full_name: [dob, gender, nationality]}
+            profile[user_id].extend([dob, gender, nationality])
 
         elif application_stage == 2:
             # full_name = input('Enter your full name')
             print('CONTACT DETAILS')
             email_address = input('Enter your email address: ')
-            phone_number = int(input('Enter your phone number:'))
+            phone_number = input('Enter your phone number:')
 
             print('\nFAMILY')
             print('i. PARENT 1: ')
             name_of_parent1 = input(('Enter the name of your parent: '))
             relation1 = input('Enter your relation with parent1: ')
-            parent_phone_number1 = int(input('Enter phone number: '))
+            parent_phone_number1 = input('Enter phone number: ')
 
             print('\nii. PARENT 2')
             name_of_parent2 = input(('Enter the name of your parent: '))
             relation2 = input('Enter your relation with parent1: ')
-            parent_phone_number2 = int(input('Enter phone number: '))
+            parent_phone_number2 = input('Enter phone number: ')
 
-            contact[id_number] = [email_address, phone_number, name_of_parent1, relation1, parent_phone_number1,
+            contact[user_id] = [email_address, phone_number, name_of_parent1, relation1, parent_phone_number1,
                                   name_of_parent2,
                                   relation2, parent_phone_number2]
 
@@ -175,17 +190,23 @@ def application_stage():
             eng_grade = input('Enter grade for English language: ')
             art_grade = input('Enter the grade for art: ')
 
-            education[id_number] = [educational_institution, level, board, math_grade, eng_grade, art_grade]
+            education[user_id] = [educational_institution, level, board, math_grade, eng_grade, art_grade]
 
         elif application_stage == 4:
             attempts = 5
             print('PERSONAL ESSAY AND ACTIVITIES')
             essay = input('Paste your personal essay that is between 100 to 200 words: ')
-            essay[id_number] = [essay]
+            personal_essay[user_id] = essay
             while attempts >= 0:
-                activities = input('Enter the activities you have been part of: ')
+                activity = input('Enter the activities you have been part of: ')
+                if activity == 'quit':
+                    break
+                if attempts == 5:
+                    activities[user_id] = [activity]
+                else:
+                    activities[user_id].append(activity)
+
                 attempts -= 1
-                activities[id_number] = [activities]
 
         elif application_stage == 5:
             print('CREATIVITY ACTIVITY')
@@ -197,5 +218,8 @@ def application_stage():
         elif application_stage == 7:
             print('Bye!!see you next time')
             break
-account_login()
-save_to_file(profile,contact, education, essay, activities)
+
+
+user_id = account_login()
+application_stage(user_id)
+save_to_file(profile, contact, education, personal_essay, activities)
